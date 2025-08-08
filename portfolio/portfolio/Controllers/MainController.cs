81f2a7a98a5f16cf596e63b2aeb947ff7e008851
Paamuru Jayannth <jayannth123good@gmail.com>
2025-08-08 10:18:16 +0530

    first commit

---
+++ portfolio/portfolio/Controllers/MainController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace portfolio.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Resume()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Send(string FirstName, string LastName, string Email, string Subject, string Message)
+        {
+            return RedirectToAction("Contact");
+        }
+
+        public IActionResult Download()
+        {
+            return File("~/File/Jayannth Resume.pdf", "application/pdf", "Pamuru_Jayannth_Resume.pdf");
+        }
+
+        public IActionResult Experience()
+        {
+            return View();
+        }
+    }
+}
